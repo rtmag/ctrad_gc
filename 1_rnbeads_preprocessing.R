15449f76d24a2168d0caba3fee0eb9c137440352
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-25 15:50:46 +0800

    Update 1_rnbeads_preprocessing.R

--- 1_rnbeads_preprocessing.R
+++ 1_rnbeads_preprocessing.R
@@ -46,3 +46,40 @@ print(idat.dir)
 	save.rnb.set(rnb.set.norm,path="/home/rtm/ctrad/GC_Illumina02/DNA_Methylation/rnb.set.norm.RData")
 print("DONE")
 ###############################################################################################################################
+
+suppressMessages(library(RnBeads))
+rnb.set.norm=load.rnb.set("rnb.set.norm.RData.zip")
+rnb.set.norm_no12=remove.samples(rnb.set.norm,samples(rnb.set.norm)[which((rnb.set.norm@pheno$Tumor) %in% 1:2)])
+TvsN_dmc <- rnb.execute.computeDiffMeth(rnb.set.norm_no12,pheno.cols=c("Tumor"))
+#
+comparison <- get.comparisons(TvsN_dmc)[1]
+noNormal_dmr_table <-get.table(TvsN_dmc, comparison, "sites", return.data.frame=TRUE)
+
+meth.norm<-meth(rnb.set.norm)
+
+colnames(meth.norm) = as.character(rnb.set.norm@pheno$Tumor)
+rownames(meth.norm) = rownames(rnb.set.norm@sites)
+
+meth.norm.sig=meth.norm[noNormal_dmr_table$diffmeth.p.adj.fdr<0.05 & abs(noNormal_dmr_table[,3])>.15,]
+
+options(scipen=999)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+
+track=colnames(meth.norm.sig)
+track[track=="N"]=1
+track[track=="T"]=2
+track[track=="1"]=3
+track[track=="2"]=3
+track=as.numeric(track)
+
+colores=c("#db4e68","#497bd1","#d1c349")
+clab=as.character(colores[track])
+
+colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
+meth.norm.sig = meth.norm.sig[complete.cases(meth.norm.sig),]
+pdf("heatmap.pdf")
+x = heatmap.2(as.matrix(meth.norm.sig),col=colors,scale="none", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,labCol = "",xlab="", ylab="CpGs",key.title="Methylation lvl",ColSideColors=clab)
+dev.off()
